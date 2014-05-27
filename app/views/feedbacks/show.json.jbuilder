10176e833a125416ec0b2e73593b767148ed6935
Prelang Builder <builder@prelang.com>
2014-05-27 21:28:52 +0000

    Generated scaffolding for model 'Feedback' via Rails

---
+++ app/views/feedbacks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @feedback, :id, :update_id, :user_id, :description, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FoundersAssembly::Application.routes.draw do
+  resources :feedbacks
+
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
